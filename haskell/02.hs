d2b84b25cdcbf2cd3d2d14330011b16f807f81b0
Ashton Baker <ashtonsb@umich.edu>
2018-12-04 11:31:29 -0500

    Finished Day 2

--- haskell/02.hs
+++ haskell/02.hs
@@ -1,22 +1,35 @@
-module Main (main) where
+module Problem02 (solve02) where
 
 import Advent (getInput)
-import Data.List (sort, group)
+import Data.List
 
-main :: IO ()
+solve02 :: IO ()
 
-main =
+solve02 =
   do input <- parseInput <$> getInput 2
      print (part1 input)
+     print (part2 input)
 
 parseInput :: String -> [String]
 parseInput = lines
 
 part1 :: [String] -> Int
-part1 s = (sum $ map (fromEnum . (hasExactlyN 2)) s) * (sum $ map (fromEnum . (hasExactlyN 3)) s)
+part1 s = (getStringsWithN 2 s) * (getStringsWithN 3 s)
+
+part2 :: [String] -> String
+part2 list =  uncurry intersect $ head [pair | pair <- pairs list, (uncurry distance $ pair) == 1]
 
 getLetterCounts :: String -> [Int]
 getLetterCounts = map length . group . sort
 
 hasExactlyN :: Int -> String -> Bool
 hasExactlyN n s = elem n $ getLetterCounts s
+
+getStringsWithN :: Int -> [String] -> Int
+getStringsWithN n s = sum $ map (fromEnum . (hasExactlyN n)) s
+
+distance :: Eq a => [a] -> [a] -> Int
+distance s1 s2 = sum . map fromEnum $ zipWith (/=) s1 s2
+
+pairs :: [a] -> [(a, a)]
+pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
